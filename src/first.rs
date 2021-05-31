99f0c87575f466db2b3687af6b996712107fb9bd
Zhongren Shao <shao.zhongren@gmail.com>
2021-05-31 17:27:19 -0500

    done with first exercise

--- src/first.rs
+++ src/first.rs
@@ -1,8 +1,9 @@
 // List a = Empty | Elem a (List a)
 
-pub enum List {
+#[derive(Debug)]
+pub enum List1 {
     Empty,
-    Elem(i32, Box<List>),
+    Elem(i32, Box<List1>),
 }
 
 // small issue, first element will be on the stack
@@ -10,3 +11,36 @@ pub enum List {
 // () = Heap
 
 // [Elem A, ptr] -> (Elem B, ptr) -> (Empty, *junk*)
+
+// really bad idea
+pub enum List2 {
+    Empty,
+    ElemThenEmpty(i32),
+    ElemThenNotEmpty(i32, Box<List2>),
+}
+
+// need everything to be pub
+pub struct Node1 {
+    elem: i32,
+    next: List3,
+}
+
+pub enum List3 {
+    Empty,
+    More(Box<Node1>),
+}
+
+// final implementation
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
