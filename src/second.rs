d49078809643b1f1a077ef1f0144bae88531ddf9
Zhongren Shao <shao.zhongren@gmail.com>
2021-05-31 19:10:39 -0500

    added peek and tests

--- src/second.rs
+++ src/second.rs
@@ -32,6 +32,21 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    // pub fn peek(&self) -> Option<&T> {
+    //     self.head.map(|node| &node.elem)
+    // }
+    // cannot move out of `self.head` which is behind a shared reference
+    // cannot return reference to local data `node.elem`
+    // map takes self by value, which will move the Option out, but now we want to leave the Option instead of take()
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -75,5 +90,24 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        // list.peek_mut().map(|&mut value| value = 42);
+        // The compiler is complaining that value is immutable, but we pretty clearly wrote &mut value; what gives? It turns out that writing the argument of the closure that way doesn't specify that value is a mutable reference. Instead, it creates a pattern that will be matched against the argument to the closure; |&mut value| means "the argument is a mutable reference, but just copy the value it points to into value, please." If we just use |value|, the type of value will be &mut i32 and we can actually mutate the head:
+        list.peek_mut().map(|value| *value = 42);
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 }
