fce6a4a5fe1713b39bb8c6bbcffb196800e91e19
Zhongren Shao <shao.zhongren@gmail.com>
2021-05-31 19:38:53 -0500

    added impl for iterator

--- src/second.rs
+++ src/second.rs
@@ -58,6 +58,31 @@ impl<T> Drop for List<T> {
     }
 }
 
+// this is a tuple struct, just wraps around List<T>
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of a tuple struct numerically
+        self.0.pop()
+    }
+}
+
+// above is not technically required, but nice to have
+// impl<T> Iterator for List<T> {
+//     type Item = T;
+//     fn next(&mut self) -> Option<Self::Item> {
+//         self.pop()
+//     }
+// }
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -113,5 +138,19 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
