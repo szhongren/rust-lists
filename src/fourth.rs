3675639eede180f2d36852cc51329648f7f0de76
Zhongren Shao <shao.zhongren@gmail.com>
2021-05-31 23:04:04 -0500

    added pop_front

--- src/fourth.rs
+++ src/fourth.rs
@@ -46,4 +46,66 @@ impl<T> List<T> {
             }
         }
     }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head);
+                }
+                None => {
+                    self.tail.take();
+                }
+            };
+            // Ah dang. into_inner wants to move out the RefCell, but we can't, because it's in an Rc.
+            // As we saw in the previous chapter, Rc<T> only lets us get shared references into its internals.
+            // That makes sense, because that's the whole point of reference counted pointers: they're shared!
+            // old_head.elem
+            // old_head.borrow_mut().elem
+            // old_head.into_inner().elem
+            // Rc::try_unwrap(old_head).unwrap().into_inner().elem
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_front(), None);
+
+        // Populate list
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_front(4);
+        list.push_front(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+    }
 }
